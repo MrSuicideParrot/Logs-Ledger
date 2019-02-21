cb1c7ce5b31ca4de89300e6fedd5bf80e4fedbd8
André Cirne <ancirne@gmail.com>
2019-02-21 12:23:13 +0000

    Continuação do bloco e limpeza de tralha

--- src/pt/up/fc/dcc/sds/group/a/consensus/Block.java
+++ src/pt/up/fc/dcc/sds/group/a/consensus/Block.java
@@ -1,17 +1,26 @@
 package pt.up.fc.dcc.sds.group.a.consensus;
 
+import pt.up.fc.dcc.sds.group.a.tools.Sha256;
+
+import java.nio.ByteBuffer;
 import java.time.Instant;
 
 public abstract class Block {
     private Block parent;
     private long timeSinceEpoch;
     private byte[] data;
+    private int index;
 
     Block(Block parent, byte[] data){
         timeSinceEpoch = Instant.now().getEpochSecond();
         this.parent = parent;
+        this.index = this.parent.getIndex() + 1;
         this.data = data;
 
+    }
+
+    int getIndex(){
+        return index;
     }
 
     byte[] getParentHash(){
@@ -23,7 +32,23 @@ public abstract class Block {
     }
 
     byte[] getHash(){
+        return Sha256.hash(this.toString().getBytes());
+    }
+
+    byte[] packBlock(){
+        //TODO: https://stackoverflow.com/questions/3209898/java-equivalent-of-pythons-struct-pack
+        //ByteBuffer block = ByteBuffer.allocate();
+        return null;
+
+    }
+
+    static Block unpackBlock(byte[] array){
+        //TODO: https://stackoverflow.com/questions/3209898/java-equivalent-of-pythons-struct-pack
         return null;
     }
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
