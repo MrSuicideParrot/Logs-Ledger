4775cfa885c7f83821f1b603e7f3e9d4de02462a
André Cirne <ancirne@gmail.com>
2019-02-28 23:58:58 +0000

    Limpeza e introdução e colocar o maven

---
+++ src/main/java/pt/up/fc/dcc/ssd/a/tracker/Tracker.java
@@ -0,0 +1,9 @@
+package pt.up.fc.dcc.ssd.a.tracker;
+
+public class Tracker {
+
+    public static void main(String [] args) {
+
+
+    }
+}
--- src/pt/up/fc/dcc/sds/group/a/Main.java
+++
@@ -1,8 +0,0 @@
-package pt.up.fc.dcc.sds.group.a;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
